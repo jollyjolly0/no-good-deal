7d1f2ba3e7d0d8055c47a7ce0f50fb2c9b576b25
LFalcone <lfcroan@gmail.com>
2022-02-06 15:04:20 -0800

    Create AudioManager prefab

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    [Header("Character")]
+    public AudioClip[] footsteps;
     public AudioClip bell;
+
+    [Header("Game State")]
     public AudioClip gameOver;
     public AudioClip win;
+
+    [Header("UI")]
     public AudioClip hover;
     public AudioClip submit;
     public AudioClip questAccepted;
     public AudioClip questDenied;
     public AudioClip questComplete;
 
+    [Header("Sources")]
     public AudioSource sfxSource;
 
     public static AudioManager Instance;
