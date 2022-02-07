6491b505227017cb432b0e66ae3edddf5b0160e0
Jake Terpening <jaketerpening@gmail.com>
2022-02-06 16:24:28 -0800

    Some work on completing the ritual

---
+++ Assets/Scripts/RitualBowlInteractable.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RitualBowlInteractable : BaseInteractable
+{
+    public bool filled;
+
+    public SpriteRenderer spriteRenderer;
+
+    public Sprite filledSprite;
+
+    public override string GetActionName()
+    {
+        return "FillRitualBowl";
+    }
+    public override void Interact(GameObject actor)
+    {
+        base.Interact(actor);
+        spriteRenderer.sprite = filledSprite;
+        filled = true;
+    }
+}
